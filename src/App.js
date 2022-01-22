6aa3ba6199cbe4c9ad39a49009139f0e710e5a03
JuliiaHrytsiuk <tuuspoliglotus@gmail.com>
2022-01-23 01:36:05 +0200

    Mount changed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import Form from "./Form";
 import Filter from "./Filter";
 import ContactList from "./ContactList";
 import { nanoid } from "nanoid";
-
-//     { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-//     { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-//     { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-//     { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+// import initialContacts from "./contacts.json";
 
 class App extends Component {
   state = {
@@ -23,10 +19,8 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const contacts = localStorage.getItem("contacts");
-    const parsedContacts = JSON.parse(contacts);
-
-    this.setState({ contacts: parsedContacts });
+    const contacts = JSON.parse(localStorage.getItem("contacts"));
+    this.setState({ contacts });
   }
 
   formSubmitHandler = ({ name, number }) => {
